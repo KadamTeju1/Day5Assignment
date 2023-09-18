cff2c8ef31d67a4425773cc454cedd161c90514d
KadamTeju <tejushinde8897@gmail.com>
2023-09-18 19:08:02 +0530

    day 5 assignment

---
+++ Day5assignment/src/day5assignment/ComputeQuotientRemainder.java
@@ -0,0 +1,14 @@
+package day5assignment;
+
+public class ComputeQuotientRemainder {
+    public static void main(String[] args) {
+
+        int dividend = 30, divisor = 4;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
